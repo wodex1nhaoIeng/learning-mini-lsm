a8fae6e22be1578193f78c4d2d1fcb11d948af9d
zhaojieyou <zjyscu@gmail.com>
2025-03-01 02:15:50 +0800

    tiered compaction

--- mini-lsm-starter/src/compact/tiered.rs
+++ mini-lsm-starter/src/compact/tiered.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::HashSet;
+
 use serde::{Deserialize, Serialize};
 
 use crate::lsm_storage::LsmStorageState;
@@ -44,7 +46,55 @@ impl TieredCompactionController {
         &self,
         _snapshot: &LsmStorageState,
     ) -> Option<TieredCompactionTask> {
-        unimplemented!()
+        if _snapshot.levels.len() < self.options.num_tiers {
+            return None;
+        }
+
+        let mut previous_size = 0;
+        for i in 0.._snapshot.levels.len() - 1 {
+            previous_size += _snapshot.levels[i].1.len();
+        }
+        println!("{:?}", self.options);
+        if previous_size as f64 / _snapshot.levels.last().unwrap().1.len() as f64
+            >= self.options.max_size_amplification_percent as f64 * 0.01 {
+            return Some(
+                TieredCompactionTask {
+                    tiers: _snapshot.levels.clone(),
+                    bottom_tier_included: true,
+                }
+            );
+        }
+
+        previous_size = 0;
+        let mut max_width_merge = 2147483647;
+        if let Some(x) = self.options.max_merge_width {
+            max_width_merge = x;
+        } 
+
+        for i in 0.._snapshot.levels.len() {
+            let this_tier_size = _snapshot.levels[i].1.len();    
+
+            if (this_tier_size as f64 / previous_size as f64) > 
+                (100.0 + self.options.size_ratio as f64) / 100.0 {
+                println!("this_tier_size: {:?}, {:?}", this_tier_size, previous_size);
+                if i >= self.options.min_merge_width && i < max_width_merge {
+                    let mut tiers = Vec::new();
+                    for j in 0..i {
+                        tiers.push(_snapshot.levels[j].clone());
+                    }
+                    return Some(
+                        TieredCompactionTask {
+                            tiers: tiers,
+                            bottom_tier_included: i == _snapshot.levels.len() - 1,
+                        }
+                    );
+                }
+            }
+
+            previous_size += this_tier_size;
+        }
+        // unimplemented!()
+        None
     }
 
     pub fn apply_compaction_result(
@@ -53,6 +103,28 @@ impl TieredCompactionController {
         _task: &TieredCompactionTask,
         _output: &[usize],
     ) -> (LsmStorageState, Vec<usize>) {
-        unimplemented!()
+        let mut snapshot = _snapshot.clone();
+        let mut files_to_remove = Vec::new();
+        let mut need_to_remove_tier = HashSet::new();
+        let mut levels = Vec::new();
+        let mut flag = false;
+
+        for i in _task.tiers.iter() {
+            need_to_remove_tier.insert(i.0);
+        }
+        for i in snapshot.levels {
+            if need_to_remove_tier.remove(&i.0) {
+                files_to_remove.extend(i.1.clone());
+            }
+            else {
+                levels.push(i.clone());
+            }
+            if need_to_remove_tier.is_empty() && flag == false {
+                flag = true;
+                levels.push((_output[0], _output.to_vec()));
+            }
+        }
+        snapshot.levels = levels;
+        (snapshot, files_to_remove)
     }
 }
